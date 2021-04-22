f1d87de95cbbf2419c33e5f83af020597742015d
Chanon Chokethaworn <chanon.cho@kbtg.tech>
2021-04-22 14:04:24 +0700

    Working with list/array

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  users: User[] = [];
+
   constructor() { }
 
+  setData(user: User): void {
+
+  }
+
   ngOnInit(): void {
+    this.users = [
+      new User(1, "User 01", "Last 01", "xxx@demo1.com"),
+      new User(2, "User 02", "Last 02", "xxx@demo2.com"),
+    ]
   }
 
 }
